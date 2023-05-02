08b3a95d60e58f6f17c21434c32a9118afbb5cde
Yang Luo <hsluoyz@qq.com>
2023-05-02 23:37:08 +0800

    Add batchEnforce() example

--- casbin.test.ts
+++ casbin.test.ts
@@ -12,3 +12,23 @@ describe('Casbin Authorization', () => {
         expect(isAllowed).toBe(true);
     });
 });
+
+describe('Casbin Batch Authorization', () => {
+    it('should authorize data access for multiple requests', async () => {
+        const enforcer = await newEnforcer('model.conf', 'policy.csv');
+
+        const requests = [
+            ['alice', 'data1', 'read'],
+            ['alice', 'data2', 'write'],
+            ['bob', 'data1', 'read'],
+            ['bob', 'data2', 'write']
+        ];
+
+        const results = await enforcer.batchEnforce(requests);
+
+        // Adjust the expected results according to your policy.
+        const expectedResults = [true, false, false, true];
+
+        expect(results).toEqual(expectedResults);
+    });
+});
