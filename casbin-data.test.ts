1e94261d3b845a1074640c44c1a03ea76ff5b4ec
Yang Luo <hsluoyz@qq.com>
2023-05-03 09:01:14 +0800

    Add comment

--- casbin-data.test.ts
+++ casbin-data.test.ts
@@ -1,23 +1,39 @@
+/* 本测试用例文件涵盖了 Casbin 的各种功能，包括基本授权、批量授权、获取用户的隐式权限以及获取用户在特定操作
+下所允许的对象条件。每个测试用例都创建了一个新的 Enforcer 对象，并使用预定义的模型和策略文件对其进行了初始化。
+在每个测试用例中，我们分别检查了请求是否被允许、结果是否符合预期等。通过这些测试用例，可以确保 Casbin 库在实际
+应用中能够按照预期工作。
+ */
+
+// 导入所需的依赖项
 import {Enforcer, newEnforcer} from 'casbin';
 import { getAllowedObjectConditions, ErrObjCondition, ErrEmptyCondition } from "./casbin-data";
 
+// 测试 Casbin 的基本授权功能
 describe('Casbin Authorization', () => {
+    // 测试一个基本的授权请求
     it('should authorize data access', async () => {
+        // 创建一个新的 Enforcer，并初始化模型和策略
         const enforcer = await newEnforcer('data/rbac_model.conf', 'data/rbac_policy.csv');
 
+        // 定义测试用的 subject, object 和 action
         const subject = 'alice';
         const object = 'data1';
         const action = 'read';
 
+        // 检查请求是否被允许
         const isAllowed = await enforcer.enforce(subject, object, action);
         expect(isAllowed).toBe(true);
     });
 });
 
+// 测试 Casbin 的批量授权功能
 describe('Casbin Batch Authorization', () => {
+    // 测试批量授权请求
     it('should authorize data access for multiple requests', async () => {
+        // 创建一个新的 Enforcer，并初始化模型和策略
         const enforcer = await newEnforcer('data/rbac_model.conf', 'data/rbac_policy.csv');
 
+        // 定义测试用的多个请求
         const requests = [
             ['alice', 'data1', 'read'],
             ['alice', 'data2', 'write'],
@@ -25,33 +41,42 @@ describe('Casbin Batch Authorization', () => {
             ['bob', 'data2', 'write']
         ];
 
+        // 执行批量授权检查
         const results = await enforcer.batchEnforce(requests);
 
-        // Adjust the expected results according to your policy.
+        // 根据策略定义预期的结果
         const expectedResults = [true, true, false, true];
 
+        // 检查结果是否符合预期
         expect(results).toEqual(expectedResults);
     });
 });
 
+// 测试获取用户的隐式权限
 describe('Casbin Get Implicit Permissions for User', () => {
+    // 测试获取用户的隐式权限
     it('should retrieve implicit permissions for a user', async () => {
+        // 创建一个新的 Enforcer，并初始化模型和策略
         const enforcer = await newEnforcer('data/rbac_model.conf', 'data/rbac_policy.csv');
 
+        // 定义测试用的用户
         const user = 'alice';
 
+        // 获取用户的隐式权限
         const permissions = await enforcer.getImplicitPermissionsForUser(user);
 
-        // Adjust the expected permissions according to your policy.
+        // 根据策略定义预期的权限
         const expectedPermissions = [
             ['alice', 'data1', 'read'],
             ['alice', 'data2', 'write']
         ];
 
+        // 检查结果是否符合预期
         expect(permissions).toEqual(expectedPermissions);
     });
 });
 
+// 定义一个辅助函数 testGetAllowedObjectConditions，用于测试获取用户在特定操作下所允许的对象条件。
 async function testGetAllowedObjectConditions(
     enforcer: Enforcer,
     user: string,
@@ -75,6 +100,7 @@ async function testGetAllowedObjectConditions(
     }
 }
 
+// 编写测试用例 'GetAllowedObjectConditions'
 describe('GetAllowedObjectConditions', () => {
     let enforcer: Enforcer;
 
@@ -82,26 +108,32 @@ describe('GetAllowedObjectConditions', () => {
         enforcer = await newEnforcer('data/object_conditions_model.conf', 'data/object_conditions_policy.csv');
     });
 
+    // 测试 Alice 在执行 'read' 操作时的允许对象条件
     test('alice read r.obj.', async () => {
         await testGetAllowedObjectConditions(enforcer, 'alice', 'read', 'r.obj.', ['price < 25', 'category_id = 2'], null);
     });
 
+    // 测试 admin 在执行 'read' 操作时的允许对象条件
     test('admin read r.obj.', async () => {
         await testGetAllowedObjectConditions(enforcer, 'admin', 'read', 'r.obj.', ['category_id = 2'], null);
     });
 
+    // 测试 Bob 在执行 'write' 操作时的允许对象条件
     test('bob write r.obj.', async () => {
         await testGetAllowedObjectConditions(enforcer, 'bob', 'write', 'r.obj.', ['author = bob'], null);
     });
 
+    // 测试 Alice 在执行 'write' 操作时的允许对象条件
     test('alice write r.obj.', async () => {
         await testGetAllowedObjectConditions(enforcer, 'alice', 'write', 'r.obj.', [], new ErrEmptyCondition());
     });
 
+    // 测试 Bob 在执行 'read' 操作时的允许对象条件
     test('bob read r.obj.', async () => {
         await testGetAllowedObjectConditions(enforcer, 'bob', 'read', 'r.obj.', [], new ErrEmptyCondition());
     });
 
+    // 测试 Alice 在执行 'read' 操作时的允许对象条件，但策略中存在无效的条件
     test('alice read r.obj. with invalid policy', async () => {
         const ok = await enforcer.addPolicy('alice', 'price > 50', 'read');
         if (ok) {
@@ -109,9 +141,9 @@ describe('GetAllowedObjectConditions', () => {
         }
     });
 
+    // 测试使用不同的用户来获取允许的对象条件
     test('prefix test', async () => {
         await enforcer.clearPolicy();
-        // await enforcer.deleteLink('alice', 'admin');
         const ok = await enforcer.addPolicies([
             ['alice', 'r.book.price < 25', 'read'],
             ['admin2', 'r.book.category_id = 2', 'read'],
@@ -119,6 +151,7 @@ describe('GetAllowedObjectConditions', () => {
         ]);
 
         if (ok) {
+            // 使用不同的用户来测试获取允许的对象条件
             await testGetAllowedObjectConditions(enforcer, 'alice', 'read', 'r.book.', ['price < 25'], null);
             await testGetAllowedObjectConditions(enforcer, 'admin2', 'read', 'r.book.', ['category_id = 2'], null);
             await testGetAllowedObjectConditions(enforcer, 'bob', 'write', 'r.book.', ['author = bob'], null);
--- casbin-query.test.ts
+++ casbin-query.test.ts
@@ -1,3 +1,10 @@
+/* 上述测试用例主要包括两部分：解析条件（parseCondition）和获取用户允许的记录（GetAllowedRecordsForUser）。
+在解析条件部分，我们针对不同的条件字符串进行测试，验证 parseCondition 函数是否能正确地解析这些字符串并
+输出相应的 Sequelize 查询条件。在获取用户允许的记录部分，我们首先创建一个新的 Enforcer 实例，然后在每个
+测试用例执行之前同步数据库并创建一些示例数据。接下来，我们测试 Alice 用户的读取权限，获取允许的对象条件，
+并使用 CombineType.OR 和 CombineType.AND 对条件进行组合，最后查询符合条件的图书数据。
+ */
+
 import {Enforcer, newEnforcer} from 'casbin';
 import { Book } from './book.model';
 import { sequelize } from './sequelize-setup';
@@ -5,7 +12,9 @@ import { Op } from 'sequelize';
 import { getAllowedObjectConditions } from './casbin-data';
 import {conditionsToSequelizeQuery, CombineType, parseCondition} from './casbin-query';
 
+// 解析条件测试用例
 describe('parseCondition', () => {
+    // 测试解析 "price < 25" 条件
     test('should parse "price < 25" condition', () => {
         const input = 'price < 25';
         const expectedOutput = {
@@ -18,6 +27,7 @@ describe('parseCondition', () => {
         expect(result).toEqual(expectedOutput);
     });
 
+    // 测试解析 "category_id = 2" 条件
     test('should parse "category_id = 2" condition', () => {
         const input = 'category_id = 2';
         const expectedOutput = {
@@ -31,13 +41,16 @@ describe('parseCondition', () => {
     });
 });
 
+// 获取用户允许的记录测试用例
 describe('GetAllowedRecordsForUser', () => {
     let enforcer: Enforcer;
 
+    // 在所有测试用例执行前创建新的 Enforcer 实例
     beforeAll(async () => {
         enforcer = await newEnforcer('data/object_conditions_model.conf', 'data/object_conditions_policy.csv');
     });
 
+    // 在每个测试用例执行前重新同步数据库并创建示例数据
     beforeEach(async () => {
         await sequelize.sync({ force: true });
         await Book.bulkCreate([
@@ -47,18 +60,22 @@ describe('GetAllowedRecordsForUser', () => {
         ]);
     });
 
+    // 测试 Alice 用户的读取权限
     test('alice read r.obj.', async () => {
         const user = 'alice';
         const action = 'read';
         const prefix = 'r.obj.';
 
+        // 获取允许的对象条件
         const conditions = await getAllowedObjectConditions(enforcer, user, action, prefix);
         console.log(conditions);
 
+        // 使用 CombineType.OR 组合条件
         console.log('CombineType.OR');
         const booksOr = await Book.findAll({ where: conditionsToSequelizeQuery(conditions, CombineType.OR) });
         console.log(booksOr.map((book) => book.toJSON()));
 
+        // 使用 CombineType.AND 组合条件
         console.log('CombineType.AND');
         const booksAnd = await Book.findAll({ where: conditionsToSequelizeQuery(conditions, CombineType.AND) });
         console.log(booksAnd.map((book) => book.toJSON()));
